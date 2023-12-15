fd6688af0e93737e7a8cf2600c401ded8e16a8d1
akandeseun <akandeseun44@gmail.com>
2023-12-15 10:26:14 +0100

    pre

--- index.php
+++ index.php
@@ -5,16 +5,35 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+
+  <style>
+    body {
+      font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+      font-weight: bold;
+    }
+
+    .box {
+      width: 100px;
+      height: 100px;
+      margin-bottom: 10px;
+    }
+  </style>
 </head>
 
 <body>
   <?php
 
-  // rgb(red, green, blue)
-  $rgb = [255, 255, 255];
   $randomIndex = rand(0, 2);
-  $randomColorStart = dechex(rand(0, 255));
-  $randomColorEnd = dechex(rand(0, 255));
+  $randomColorStart = dechex(rand(0, 200));
+  $randomColorEnd = dechex(rand(0, 200));
+
+  if (strlen($randomColorStart) == 1) {
+    $randomColorStart .= $randomColorStart;
+  }
+
+  if (strlen($randomColorEnd) == 1) {
+    $randomColorEnd .= $randomColorEnd;
+  }
 
   $hexCode = [00, 00, 00];
 
@@ -23,15 +42,15 @@
   $hexcodes = [];
 
   // 1 - red, 2 - blue, 3 - green
-  $colorsHex = [
-    1 => [],
-    2 => [],
-    3 => []
-  ];
+  // $colorsHex = [
+  //   1 => [],
+  //   2 => [],
+  //   3 => []
+  // ];
 
   // populate color numbers array with 64 random numbers between 0 - 255
   for ($i = 0; $i < 64; $i++) {
-    $randomValues = rand(0, 255);
+    $randomValues = rand(10, 200);
     $colorNumbers[$i] = $randomValues;
 
     // $colorsHex[$randomIndex][$i] = $randomValues;
@@ -39,10 +58,11 @@
 
   rsort($colorNumbers);
 
-  for ($i = 0; $i < 64; $i++) {
+  for ($i = 0; $i < 62; $i++) {
     $x = dechex($colorNumbers[$i]);
     if (strlen($x) == 1) {
       $x .= $x;
+      // $hexcodes[$i] = $x;
     }
 
     $hexcodes[$i] = $x;
@@ -53,11 +73,12 @@
 
 
   <ol>
-    <?php foreach ($hexcodes as $color) : ?>
+    <?php foreach (array_unique($hexcodes) as $color) : ?>
 
-      <?php $fullColor = "#$randomColorStart" . "$randomColorEnd" . "$color"; ?>
+      <?php $fullColor = "#$randomColorStart" . "$color" . "$randomColorEnd"; ?>
 
-      <li style="color: <?= $fullColor ?>">something</li>
+      <div class="box" style="background:<?= $fullColor ?>"></div>
+      <!-- <p><?= $fullColor ?></p> -->
     <?php endforeach; ?>
   </ol>
 
